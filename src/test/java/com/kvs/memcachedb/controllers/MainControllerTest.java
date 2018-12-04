9c3f5b273b44272b13eb465974b4215bada77395
Aleksei Kruglik <aleksei_kruglik@epam.com>
2018-12-04 17:02:28 +0300

    adds open street api

--- src/test/java/com/kvs/memcachedb/controllers/MainControllerTest.java
+++ src/test/java/com/kvs/memcachedb/controllers/MainControllerTest.java
@@ -4,6 +4,8 @@ import au.com.bytecode.opencsv.CSVReader;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.stream.JsonReader;
+import com.kvs.memcachedb.entity.Sight;
+import com.kvs.memcachedb.utils.OpenStreetMapUtils;
 import org.junit.Test;
 import org.w3c.dom.Document;
 
@@ -17,7 +19,9 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -25,11 +29,50 @@ public class MainControllerTest {
 
     @Test
     public void loadDataset() throws Exception {
+
+
+        ArrayList<Sight> sights = new ArrayList<>();
         CSVReader reader = new CSVReader(new FileReader("src/main/resources/Data.csv"), ',' , '"' , 1);
+
         String[] nextLine;
-//        while ((nextLine = reader.readNext()) != null) {
-//            System.out.println(Arrays.toString(nextLine));
-//        }
+        Map<String, Double> coords;
+        int i = 0;
+        StringBuilder result = new StringBuilder("");
+        while ((nextLine = reader.readNext()) != null) {
+            try (FileWriter writer = new FileWriter(new File("C:\\Users\\Aleksei_Kruglik\\Desktop\\study\\nosql\\nosql2018-memcached_routing\\src\\main\\resources\\templates\\coords"))){
+            Sight sight = new Sight();
+            sight.setEnsembleName(nextLine[1]);
+            sight.setObjectName(nextLine[2]);
+            sight.setDate(nextLine[3]);
+            sight.setAuthors(nextLine[4]);
+            sight.setAddress(nextLine[5]);
+            sight.setDistrict(nextLine[6]);
+            sight.setBase(nextLine[8]);
+            sight.setDescription(nextLine[9]);
+            sights.add(sight);
+            String address = "Санкт-Петербург " + sight.getAddress();
+
+            Thread.sleep(1500);
+
+            coords = OpenStreetMapUtils.getInstance().getCoordinates(address);
+                result = new StringBuilder("");
+
+                result.append(i + 1)
+                      .append(":")
+                      .append(sight.getAddress())
+                      .append(":")
+                      .append(coords.get("lat"))
+                      .append(":")
+                      .append(coords.get("lon"))
+                      .append("\n");
+
+            ++i;
+                writer.write(result.toString());
+            } catch (Exception ex){
+                ex.printStackTrace();
+            }
+        }
+
 
 
     }
